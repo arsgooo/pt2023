005635329947e715637ea613a699f2fd7f3f3023
Arsenii Dobrovolskyi <cleverboyars@gmail.com>
2023-10-16 14:48:51 +0300

    +

--- Lab2/pawnshop.py
+++ Lab2/pawnshop.py
@@ -7,8 +7,14 @@ def add_good(goods, name, price):
 
 def remove_good(goods, name):
     if name in goods:
-        del goods[name]
-        return goods
+        removed_good = goods.pop(name)
+        return goods, removed_good
+    else:
+        raise ValueError(f"The good '{name}' is not found")
+
+def get_good(goods, name):
+    if name in goods:
+        return (name, goods[name])
     else:
         raise ValueError(f"The good '{name}' is not found")
 
@@ -18,4 +24,3 @@ def get_goods_amount(goods):
 def get_total_cost(goods):
     total_cost = sum(goods.values())
     return total_cost
-
--- Lab2/pawnshop_tests.py
+++ Lab2/pawnshop_tests.py
@@ -3,7 +3,7 @@ import pawnshop as ps
 
 class TestGoodsFunctions(unittest.TestCase):
     def setUp(self):
-        self.goods = {"Radio": 200, "Headphones": 300}
+        self.goods = {"Radio": 200, "Headphones": 300, "Watch": 499.55555, "Ring": 749.47755}
 
     def test_add_good_normal(self):
         ps.add_good(self.goods, "Laptop", 1000)
@@ -22,13 +22,21 @@ class TestGoodsFunctions(unittest.TestCase):
         with self.assertRaises(ValueError): #trying to remove radio again and facing error
             ps.remove_good(self.goods, "Radio")
 
+    def test_get_good(self):
+        result = ps.get_good(self.goods, "Headphones")
+        self.assertEqual(result, ("Headphones", 300)) #checking that the correct good is returned
+
+        with self.assertRaises(ValueError):
+            result = ps.get_good(self.goods, "Laptop") #trying to get the good that is absent in the dictionary
+
     def test_get_goods_amount(self):
         amount = ps.get_goods_amount(self.goods)
-        self.assertEqual(amount, 2)
+        self.assertEqual(amount, 4)
 
     def test_get_total_cost(self):
+        expected_total_cost = round(sum(self.goods.values()), 2)
         total_cost = ps.get_total_cost(self.goods)
-        self.assertEqual(total_cost, 500)
+        self.assertAlmostEqual(total_cost, expected_total_cost, places=2)
 
 if __name__ == "__main__":
     unittest.main()
