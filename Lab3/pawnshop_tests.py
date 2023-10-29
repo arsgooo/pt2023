fc499806e639c270f50e0597a97b61aeb2bcac87
Arsenii Dobrovolskyi <cleverboyars@gmail.com>
2023-10-29 14:52:42 +0200

    +

--- Lab3/pawnshop_tests.py
+++ Lab3/pawnshop_tests.py
@@ -4,7 +4,12 @@ import pawnshop as ps
 
 class TestGoodsFunctions(unittest.TestCase):
     def setUp(self):
-        self.goods = {"Radio": 200, "Headphones": 300, "Watch": 499.55555, "Ring": 749.47755}
+        self.goods = {
+            "Radio": 200,
+            "Headphones": 300,
+            "Watch": 499.55555,
+            "Ring": 749.47755,
+        }
 
     def test_add_good_normal(self):
         ps.add_good(self.goods, "Laptop", 1000)
@@ -17,18 +22,26 @@ class TestGoodsFunctions(unittest.TestCase):
         self.assertTrue(ps.add_good(self.goods, 500, 400))
 
     def test_remove_good(self):
-        ps.remove_good(self.goods, "Radio")  # removing radio and checking that it doesn't exist anymore
+        ps.remove_good(
+            self.goods, "Radio"
+        )  # removing radio and checking that it doesn't exist anymore
         self.assertNotIn("Radio", self.goods)
 
-        with self.assertRaises(ValueError):  # trying to remove radio again and facing error
+        with self.assertRaises(
+            ValueError
+        ):  # trying to remove radio again and facing error
             ps.remove_good(self.goods, "Radio")
 
     def test_get_good(self):
         result = ps.get_good(self.goods, "Headphones")
-        self.assertEqual(result, ("Headphones", 300))  # checking that the correct good is returned
+        self.assertEqual(
+            result, ("Headphones", 300)
+        )  # checking that the correct good is returned
 
         with self.assertRaises(ValueError):
-            result = ps.get_good(self.goods, "Laptop")  # trying to get the good that is absent in the dictionary
+            result = ps.get_good(
+                self.goods, "Laptop"
+            )  # trying to get the good that is absent in the dictionary
 
     def test_get_goods_amount(self):
         amount = ps.get_goods_amount(self.goods)
@@ -40,7 +53,7 @@ class TestGoodsFunctions(unittest.TestCase):
         self.assertAlmostEqual(total_cost, expected_total_cost, places=2)
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     import xmlrunner
 
     runner = xmlrunner.XMLTestRunner(output="test-reports")
